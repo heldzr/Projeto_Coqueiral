2336192114844a0e0aa57c62bfd283eacf7814bd
Lucas carneiro <lucas.carneirocontact@gmail.com>
2024-05-20 18:52:32 -0300

    criando pagina home

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INÍCIO</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/b09944a1ea.js" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+    <div class="navbar">
+        <img src="imagens/logo.png" alt="logo coqueiral" class="logo">
+        <div class="nav-icon-container">
+            <img src="imagens/ajudaazul 1.png" alt="ajuda" class="ajuda">
+            <img src="imagens/Group 159.png" alt="perfil" class="perfil">
+            <img src="imagens/Vector.png" alt="notificação" class="notificacao">
+            <img src="imagens/sacolinhascoqueiral 1.png" alt="sacola" class="sacola">
+           </div>
+            <button id="menuBtn" class="menu-button"><i class="fas fa-bars"></i></button>
+        
+        <div class="header-inner-content show-menu">
+            
+            <nav>
+                <ul>
+                    <li>INÍCIO</li>
+                    <li> - </li>
+                    <li>SOBRE</li>
+                    <li> - </li>
+                    <li>VENDER NA COQUEIRAL</li>
+                    <li> - </li>
+                    <li>ÉTICA</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+  <div class="search-container">
+    <div class="search-content">
+        <input type="text" id="search" placeholder="Pesquise o que você procura...">
+        <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+</div>
+<hr class="divider">
+<div>
+    <div class="page-inner-content">
+        <h3 class="section-title">Produtos em alta</h3>
+        <div class="subtitle-underline"></div>
+    </div>
+
+      <div class="cols cols-4">
+      <div class="product">
+        <img src="imagens/roupa1.png" alt="produto1">
+        <p class="product-name">Conjunto tie-dye - Verdin Brecho</p>
+        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+        <p class="product-price">R$120,00</p>
+      </div>
+      <div class="product">
+        <img src="imagens/roupa2.png" alt="produto1">
+        <p class="product-name">Conjunto tie-dye - Verdin Brecho</p>
+        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+        <p class="product-price">R$120,00</p>
+      </div>
+      <div class="product">
+        <img src="imagens/roupa3.png" alt="produto2">
+        <p class="product-name">Conjunto tie-dye - Verdin Brecho</p>
+        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+        <p class="product-price">R$120,00</p>
+      </div>
+      <div class="product">
+        <img src="imagens/roupa4.png" alt="produto3">
+        <p class="product-name">Conjunto tie-dye - Verdin Brecho</p>
+        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+        <p class="product-price">R$120,00</p>
+      </div>
+      <div class="product">
+        <img src="imagens/roupa1.png" alt="produto4">
+        <p class="product-name">Conjunto tie-dye - Verdin Brecho</p>
+        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+        <p class="product-price">R$120,00</p>
+      </div>
+      <div class="product">
+        <img src="imagens/roupa4.png" alt="produto4">
+        <p class="product-name">Conjunto tie-dye - Verdin Brecho</p>
+        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+        <p class="product-price">R$120,00</p>
+      </div>
+      <div class="product">
+        <img src="imagens/roupa3.png" alt="produto4">
+        <p class="product-name">Conjunto tie-dye - Verdin Brecho</p>
+        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+        <p class="product-price">R$120,00</p>
+      </div>
+      <div class="product">
+        <img src="imagens/roupa2.png" alt="produto4">
+        <p class="product-name">Conjunto tie-dye - Verdin Brecho</p>
+        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+        <p class="product-price">R$120,00</p>
+      </div>
+      </div>
+
+</div>
+       <footer> 
+        <hr> 
+        <div class="geral"> <h4>Contato</h4> 
+                <p>coqueiralrecife@contato.br</p>
+                <h4>©️ 2024 BY COQUEIRAL</h4>
+                </div>
+          </footer>
+</body>
+</html>
